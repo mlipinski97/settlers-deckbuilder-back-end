476804d2a67e2e7fa9075a82f7980bf6a2747a1d
Maciej <maciekl3131@gmail.com>
2021-03-30 12:27:42 +0200

    created project

---
+++ src/main/java/pl/lipinski/settlers_deckbuilder/SettlersDeckBuilderApplication.java
@@ -0,0 +1,13 @@
+package pl.lipinski.settlers_deckbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlersDeckBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettlersDeckBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lipinski/settlers_deckbuilder/SettlersDeckbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lipinski.settlers_deckbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlersDeckbuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
